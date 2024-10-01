19a8cb6855cda272201b506a75f4057010e1c86f
Sergei Gushin <ravenvl71@gmail.com>
2024-10-01 13:56:29 +0300

    add logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yadisk
 import os
 import argparse
 import zipfile
+import loguru
 
 from dotenv import load_dotenv
 from datetime import datetime
@@ -11,7 +12,9 @@ token = os.getenv('YANDEX_TOKEN')
 y = yadisk.YaDisk(token=token)
 parser = argparse.ArgumentParser(description='Backups files to YandexDisk')
 parser.add_argument('-d' ,'--date' , action='store_true' , help='Backups files to YandexDisk by date')
-
+logger = loguru.logger
+logger.level("INFO")
+logger.add("log_file.log", format="{time:DD-MM-YYYY HH:mm:ss} | {level} | {message}")
 def run(path, dest_dir):
     """
     Загрузка папок и файлов
@@ -20,10 +23,11 @@ def run(path, dest_dir):
     for address, dirs, files in os.walk(path):
         for dir in dirs:
             y.mkdir(f'/Резервные копии М2 Ардон-Фиш/{dest_dir}/{dir}')
-            print(f'Папка {dir} создана')
+            # print(f'Папка {dir} создана')
+            # logger.info(f'Папка {dir} создана')
         for file in files:
-            print(f'Файл {file} загружен')
             y.upload(f'{address}/{file}', f'/Резервные копии М2 Ардон-Фиш/{dest_dir}/{file}', overwrite=True, timeout=60)
+            # logger.info(f'Файл {file} загружен')
 
 def run_date(path,dest_dir, date):
     try:
@@ -37,13 +41,16 @@ def run_date(path,dest_dir, date):
     for address, dirs, files in os.walk(path):
         for dir in dirs:
             y.mkdir(f'/Резервные копии М2 Ардон-Фиш/backup/{date}/{dest_dir}/{dir}')
-            print(f'Папка {dir} создана')
+            # print(f'Папка {dir} создана')
         with zipfile.ZipFile(f'{dest_dir}.zip', 'w') as zip:
             for file in files:
-                print(f'Файл {file} загружен')
                 zip.write(f'{address}/{file}')
-        y.upload(f'{dest_dir}.zip', f'/Резервные копии М2 Ардон-Фиш/backup/{date}/{dest_dir}/{dest_dir}.zip',  overwrite=True, timeout=160)
+                # logger.info(f'Файл {file} добавлен в архив')
 
+        y.upload(f'{dest_dir}.zip', f'/Резервные копии М2 Ардон-Фиш/backup/{date}/{dest_dir}/{dest_dir}.zip',  overwrite=True, timeout=160)
+        # print(f'Файл {dest_dir}.ziр загружен')
+        logger.info(f'Файл {dest_dir}.ziр загружен')
+        os.remove(f'{dest_dir}.zip')
 def check():
     """
     Проверка токена!
@@ -52,19 +59,23 @@ def check():
 
 def main_run():
     args = parser.parse_args()
+
     date = datetime.strftime(datetime.now(), "%d.%m.%Y-%H.%M.%S")
     if args.date:
-        print('Запуск по дате')
+        logger.info("Backup by date")
         check()
         run_date(r'd:\base_spu', 'base_spu', date)
         run_date(r'd:\base', 'base', date)
     else:
-        print('Запуск по папкам')
+        logger.info("Backup by default")
         check()
         run(r'd:\base_spu', 'base_spu')
         run(r'd:\base', 'base')
-
+    logger.info(f'Резервная копия М2 Ардон-Фиш создана: {date}')
 
 
 if __name__ == '__main__':
-    main_run()
+    try:
+        main_run()
+    except Exception as e:
+        logger.error(e)
